5aff73073ed29ce5ba7f31a61b0b202b4d6558df
SomeshBanna <someshbanna845@gmail.com>
2022-11-08 10:44:46 +0530

    Test log added

--- src/test/java/com/example/crud/CrudProjectApplicationTests.java
+++ src/test/java/com/example/crud/CrudProjectApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.crud;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrudProjectApplicationTests {
-
+	public static Logger logger=LoggerFactory.getLogger(CrudProjectApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("TEST CASE EXECUTING...");
+		assertEquals(true,true);
 	}
 
 }
