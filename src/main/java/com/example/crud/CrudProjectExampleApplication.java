cc4c128d68c3a352ccb80ea87273eec268d2e3ab
SomeshBanna <someshbanna845@gmail.com>
2022-11-23 12:45:15 +0530

    test added

--- src/main/java/com/example/crud/CrudProjectExampleApplication.java
+++ src/main/java/com/example/crud/CrudProjectExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudProjectApplication {
+public class CrudProjectExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudProjectApplication.class, args);
+		SpringApplication.run(CrudProjectExampleApplication.class, args);
 	}
 
 }
